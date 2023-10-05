1d3868566772a694a3eb1f3d5ceecec5899e8df5
rpraveen2005 <147073429+rpraveen2005@users.noreply.github.com>
2023-10-05 20:06:19 +0530

    Add files via upload

---
+++ Challenge - Unit 1/factorial.py
@@ -0,0 +1,17 @@
+def factorial(n):
+
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+num = int(input("Enter a non-negative integer to calculate its factorial: "))
+
+if num < 0:
+    print("Factorial is undefined for negative numbers.")
+else:
+    result = factorial(num)
+    print(f"The factorial of {num} is {result}")
+
+
+
---
+++ Challenge - Unit 1/leap year.py
@@ -0,0 +1,6 @@
+year = int(input("Enter a year: "))
+
+leap_year = (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
+print(f"{year} is {'a leap year.' if leap_year else 'not a leap year.'}")
+
